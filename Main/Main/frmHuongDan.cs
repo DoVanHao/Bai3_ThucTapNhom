d03554769516a8bd5ab568a1f8e3dafa6597ff41
tamerlane2998 <trieuquangminh29@gmail.com>
2019-09-04 20:42:21 +0700

    Tạo Form Huongdan

---
+++ Main/Main/frmHuongDan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Main
+{
+    public partial class frmHuongDan : Form
+    {
+        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+
+      
+    }
+}
