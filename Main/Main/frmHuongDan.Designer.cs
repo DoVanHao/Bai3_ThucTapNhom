a0648713bf77c91b9050761045a2daa3649719d7
tamerlane2998 <trieuquangminh29@gmail.com>
2019-09-10 07:44:22 +0700

    Update frmHuongDan.Designer.cs

--- Main/Main/frmHuongDan.Designer.cs
+++ Main/Main/frmHuongDan.Designer.cs
@@ -90,6 +90,21 @@
             this.txtGioiThieu.Size = new System.Drawing.Size(546, 147);
             this.txtGioiThieu.TabIndex = 3;
             // 
+            // frmHuongDan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 424);
+            this.Controls.Add(this.txtGioiThieu);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "frmHuongDan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmHuongDan";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmHuongDan_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
