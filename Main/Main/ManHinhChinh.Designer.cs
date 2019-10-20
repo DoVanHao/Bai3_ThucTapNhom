cdeff0abe371343fbe1d0826ca648495e7cba3d3
MinhHieu <rezo.krifferson@gmail.com>
2019-10-20 23:40:43 +0700

    man hinh chinh

---
+++ Main/Main/ManHinhChinh.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace BTL_QLNS
+{
+    partial class ManHinhChinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNhanVien = new System.Windows.Forms.Button();
+            this.btnPhongBan = new System.Windows.Forms.Button();
+            this.btnDuan = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.PH = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNhanVien
+            // 
+            this.btnNhanVien.ForeColor = System.Drawing.Color.Red;
+            this.btnNhanVien.Location = new System.Drawing.Point(71, 232);
+            this.btnNhanVien.Name = "btnNhanVien";
+            this.btnNhanVien.Size = new System.Drawing.Size(205, 56);
+            this.btnNhanVien.TabIndex = 0;
+            this.btnNhanVien.Text = "Quản lý nhân viên";
+            this.btnNhanVien.UseVisualStyleBackColor = true;
+            this.btnNhanVien.Click += new System.EventHandler(this.btnNhanVien_Click);
+            // 
+            // btnPhongBan
+            // 
+            this.btnPhongBan.ForeColor = System.Drawing.Color.Red;
+            this.btnPhongBan.Location = new System.Drawing.Point(325, 232);
+            this.btnPhongBan.Name = "btnPhongBan";
+            this.btnPhongBan.Size = new System.Drawing.Size(208, 56);
+            this.btnPhongBan.TabIndex = 1;
+            this.btnPhongBan.Text = "Quản lý phòng ban";
+            this.btnPhongBan.UseVisualStyleBackColor = true;
+            // 
+            // btnDuan
+            // 
+            this.btnDuan.ForeColor = System.Drawing.Color.Red;
+            this.btnDuan.Location = new System.Drawing.Point(599, 232);
+            this.btnDuan.Name = "btnDuan";
+            this.btnDuan.Size = new System.Drawing.Size(205, 56);
+            this.btnDuan.TabIndex = 2;
+            this.btnDuan.Text = "Quản lý dự án";
+            this.btnDuan.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExit.Location = new System.Drawing.Point(785, 29);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(78, 32);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Trở lại";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // PH
+            // 
+            this.PH.AutoSize = true;
+            this.PH.BackColor = System.Drawing.Color.Transparent;
+            this.PH.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.PH.ForeColor = System.Drawing.Color.Black;
+            this.PH.Location = new System.Drawing.Point(172, 108);
+            this.PH.Name = "PH";
+            this.PH.Size = new System.Drawing.Size(426, 39);
+            this.PH.TabIndex = 6;
+            this.PH.Text = "Phần mềm quản lí nhân sự";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(645, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "MTA SOFTWARE";
+            // 
+            // ManHinhChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PH);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDuan);
+            this.Controls.Add(this.btnPhongBan);
+            this.Controls.Add(this.btnNhanVien);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "ManHinhChinh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Màn Hình Chính";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private void InitializeComponent11()
+        {
+          
+            // 
+            // btnNhanVien
+            // 
+            this.btnNhanVien.ForeColor = System.Drawing.Color.Red;
+            this.btnNhanVien.Location = new System.Drawing.Point(71, 232);
+            this.btnNhanVien.Name = "btnNhanVien";
+            this.btnNhanVien.Size = new System.Drawing.Size(212, 56);
+            this.btnNhanVien.TabIndex = 0;
+            this.btnNhanVien.Text = "Quản lý nhân viên";
+            this.btnNhanVien.UseVisualStyleBackColor = true;
+            this.btnNhanVien.Click += new System.EventHandler(this.btnNhanVien_Click);
+            // 
+            // btnPhongBan
+            // 
+            this.btnPhongBan.ForeColor = System.Drawing.Color.Red;
+            this.btnPhongBan.Location = new System.Drawing.Point(325, 232);
+            this.btnPhongBan.Name = "btnPhongBan";
+            this.btnPhongBan.Size = new System.Drawing.Size(212, 56);
+            this.btnPhongBan.TabIndex = 1;
+            this.btnPhongBan.Text = "Quản lý phòng ban";
+            this.btnPhongBan.UseVisualStyleBackColor = true;
+            // 
+            // btnDuan
+            // 
+            this.btnDuan.ForeColor = System.Drawing.Color.Red;
+            this.btnDuan.Location = new System.Drawing.Point(599, 232);
+            this.btnDuan.Name = "btnDuan";
+            this.btnDuan.Size = new System.Drawing.Size(212, 56);
+            this.btnDuan.TabIndex = 2;
+            this.btnDuan.Text = "Quản lý dự án";
+            this.btnDuan.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExit.Location = new System.Drawing.Point(799, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(78, 32);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Trở lại";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // PH
+            // 
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(645, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "MTA SOFTWARE";
+            // 
+            // ManHinhChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(889, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PH);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDuan);
+            this.Controls.Add(this.btnPhongBan);
+            this.Controls.Add(this.btnNhanVien);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "ManHinhChinh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Màn Hình Chính";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNhanVien;
+        private System.Windows.Forms.Button btnPhongBan;
+        private System.Windows.Forms.Button btnDuan;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label PH;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Main/Main/ManHinhChinh.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BTL_QLNS
+{
+    public partial class ManHinhChinh : Form
+    {
+        public ManHinhChinh()
+        {
+            InitializeComponent();
+        }
+        private void frm_Closed(object sender, FormClosedEventArgs e)
+        {
+            this.Show();
+        }
+        private void btnNhanVien_Click(object sender, EventArgs e)
+        {
+        //hien thi ra frm chính
+            Quanlynhanvien frmnv = new Quanlynhanvien();
+            frmnv.Show();
+            this.Hide();
+        }
+
+    }
+}
